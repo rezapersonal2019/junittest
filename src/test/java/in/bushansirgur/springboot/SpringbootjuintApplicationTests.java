ea6c2674e753a485863528b00363cf8d9fd529e9
Selim <selimr1989@gmai.com>
2022-02-24 15:48:56 -0500

    Junit test project

--- src/test/java/in/bushansirgur/springboot/SpringbootjuintApplicationTests.java
+++ src/test/java/in/bushansirgur/springboot/SpringbootjuintApplicationTests.java
@@ -24,37 +24,43 @@ class SpringbootjuintApplicationTests {
 	@Autowired
 	ProductRepo pRepo;
 	
-	/*
-	 * @Test
-	 * 
-	 * @Order(1) public void testCreate () { Product p = new Product(); p.setId(1L);
-	 * p.setName("iPhone XR"); p.setDesc("Fantastic"); p.setPrice(1000.00);
-	 * pRepo.save(p); assertNotNull(pRepo.findById(1L).get()); }
-	 */
-	/*
-	 * @Test
-	 * 
-	 * @Order(2) public void testReadAll () { List list = pRepo.findAll();
-	 * assertThat(list).size().isGreaterThan(0); }
-	 */
+	
+	// Create table in my sql server
+	
+	
+	  @Test
+	  
+	  @Order(1) public void testCreate () { Product p = new Product(); p.setId(1L);
+	  p.setName("iPhone XR"); p.setDesc("Fantastic"); p.setPrice(1000.00);
+	  pRepo.save(p); assertNotNull(pRepo.findById(1L).get()); }
+	  
+	  
+	  @Test
+	  
+	  @Order(2) public void testReadAll () { List list = pRepo.findAll();
+	  assertThat(list).size().isGreaterThan(0); }
+	 
 		
 	/*
 	 * @Test
 	 * 
-	 * @Order(3) public void testRead () { Product product =
+	 * @Order(3) 
+	 * public void testRead () { Product product =
 	 * pRepo.findById(1L).get(); assertEquals("iPhone XR", product.getName()); }
 	 */
 	  @Test
 	 
-	  @Order(4) public void testUpdate () { Product p = pRepo.findById(1L).get();
-	  p.setPrice(800.00); pRepo.save(p); assertNotEquals(700.00,
+	  @Order(4)
+	  public void testUpdate () { Product p = pRepo.findById(1L).get();
+	  p.setPrice(900.00); pRepo.save(p); assertNotEquals(800.00,
 	  pRepo.findById(1L).get().getPrice()); }
 	  
 	  
 	/*
 	 * @Test
 	 * 
-	 * @Order(5) public void testDelete () { pRepo.deleteById(1L);
+	 * @Order(5) 
+	 * public void testDelete () { pRepo.deleteById(1L);
 	 * assertThat(pRepo.existsById(1L)).isFalse(); }
 	 */
 
